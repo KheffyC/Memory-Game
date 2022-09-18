3eb7412fd39d17b1995362aa00683db5f5832992
KheffyC <kheffy.cervantez@gmail.com>
2022-09-17 23:33:54 -0700

    cache board elements and render tiles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 /*----- constants -----*/
-const startButton = document.querySelector(".start-button");
-const introScreen = document.querySelector(".intro-screen");
-const mainScreen = document.querySelector(".main");
+const boardObj = {
+    "easy": 4,
+    "medium": 5,
+    "hard": 6
+}
 
 
 
@@ -11,7 +13,10 @@ const mainScreen = document.querySelector(".main");
 
 
 /*----- cached element references -----*/
-
+const startButton = document.querySelector(".start-button");
+const introScreen = document.querySelector(".intro-screen");
+const mainScreen = document.querySelector(".main");
+const board = document.querySelector(".board");
 
 
 
@@ -28,4 +33,20 @@ function handleStartButton(){
     mainScreen.style.display = 'flex'
     document.querySelector('.header p').style.display = 'block'
     document.querySelector('.header-score').style.display = 'block'
+    createBoard()
 }
+
+function createBoard(){
+    // create board tiles
+    let mulitplyTilesArr = new Array(boardObj['medium'] * boardObj['medium'])
+    for (let i = 0; i < mulitplyTilesArr.length; i++){
+        let newTile = document.createElement('div');
+        newTile.id = i;
+        newTile.className = "board-tile";
+        console.log(i, newTile)
+        board.appendChild(newTile)
+    }
+    console.log(board, "board")
+}
+
+createBoard()
