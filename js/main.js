72141ce8d84a9c54654fda58ab11bc06df26d049
KheffyC <kheffy.cervantez@gmail.com>
2022-09-18 22:51:00 -0700

    create Levels and render Chosen Tiles

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 /*----- constants -----*/
-const boardObj = {
-    "easy": 4,
-    "medium": 5,
-    "hard": 6
-}
+const boardLevels = [
+    {
+        'easy': 4,
+        'min-size': 25,
+        'maxGuesses': 6
+    },
+    {
+        'medium': 5,
+        'min-size': 20,
+        'maxGuesses': 10
+    },
+    {
+        'hard': 6,
+        'min-size': 16,
+        'maxGuesses': 16
+    }
+]
+
 
 
 
 /*----- app's state (variables) -----*/
-
+let levelFinder, boardAdjuster
 
 
 
@@ -17,13 +30,13 @@ const startButton = document.querySelector(".start-button");
 const introScreen = document.querySelector(".intro-screen");
 const mainScreen = document.querySelector(".main");
 const board = document.querySelector(".board");
-
+const dropdown = document.querySelector(".levels");
 
 
 
 /*----- event listeners -----*/
 startButton.addEventListener('click', handleStartButton)
-
+dropdown.addEventListener('change', changeLevels)
 
 
 
@@ -33,20 +46,46 @@ function handleStartButton(){
     mainScreen.style.display = 'flex'
     document.querySelector('.header p').style.display = 'block'
     document.querySelector('.header-score').style.display = 'block'
+    init()
+}
+
+function init(){
     createBoard()
+    chosenTiles()
+}
+
+function changeLevels(){
+     levelFinder = dropdown.value;
+     startButton.style.visibility = "visible"
+     if(dropdown.value === 'easy'){
+        boardAdjuster = boardLevels[0]
+     } else if (dropdown.value === 'medium'){
+        boardAdjuster = boardLevels[1]
+     } else if (dropdown.value === 'hard'){
+        boardAdjuster = boardLevels[2]
+     }
 }
 
 function createBoard(){
     // create board tiles
-    let mulitplyTilesArr = new Array(boardObj['medium'] * boardObj['medium'])
+    let mulitplyTilesArr = new Array(boardAdjuster[`${levelFinder}`] * boardAdjuster[`${levelFinder}`])
     for (let i = 0; i < mulitplyTilesArr.length; i++){
         let newTile = document.createElement('div');
         newTile.id = i;
         newTile.className = "board-tile";
-        console.log(i, newTile)
+        newTile.style.minHeight = `${boardAdjuster['min-size']}%`
+        newTile.style.minWidth = `${boardAdjuster['min-size']}%`
         board.appendChild(newTile)
     }
-    console.log(board, "board")
 }
 
-createBoard()
+function chosenTiles(){
+    let memoryTile = boardAdjuster['maxGuesses']
+    let tileMax = boardAdjuster[`${dropdown.value}`] * boardAdjuster[`${dropdown.value}`]
+    let chosenTileArr = []
+    for (let i=0; i < memoryTile; i++){
+        chosenTileArr.push(Math.floor(Math.random() * tileMax))
+        i;
+    }
+    chosenTileArr.forEach(tile => document.getElementById(`${tile}`).style.backgroundColor = "blue")
+}
